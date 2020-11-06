dff149d5e2315c2024775895000b1f5070bf2fbc
abuelregal7 <abuyasen@yahoo.com>
2020-11-07 00:37:38 +0200

    Initial Commit

---
+++ NetworkLayerAlamofireWithCodable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLayerAlamofireWithCodable
+//
+//  Created by Ahmed on 11/7/20.
+//  Copyright © 2020 Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
